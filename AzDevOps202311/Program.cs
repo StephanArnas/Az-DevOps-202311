9cf5b7e513fcf279fb08a8d9f161d14c27f05366
stephan.arnas@gmail.com <stephan.arnas@gmail.com>
2023-11-27 12:28:11 +0100

    ajout de commentaire sur program 2

--- AzDevOps202311/Program.cs
+++ AzDevOps202311/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Add comment to test commit.
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
